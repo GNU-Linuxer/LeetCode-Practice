d093eb29aa82f1a454488c533b5cdf6f329fec44
David Xie <xdkdavid@gmail.com>
2022-02-12 02:14:55 -0800

    Update Q98M

---
+++ OtherApproach/Q98M_2.java
@@ -0,0 +1,44 @@
+// 98. Validate Binary Search Tree (Medium)
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+// Approach of online Bilibili video, using recursions calls similar to binary search
+public class Q98M_2 {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long low, long high) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val > low && root.val < high) {
+            return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, high);
+        } else {
+            return false;
+        }
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
